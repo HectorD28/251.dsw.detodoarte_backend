219ad3b4f5d6bd7372371aed2e324c3e7674e7c9
hector <hectorda28yo@gmail.com>
2025-05-09 11:51:27 -0500

    Inicialización del repositorio

---
+++ src/test/java/dsw/detodoartebackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dsw.detodoartebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
