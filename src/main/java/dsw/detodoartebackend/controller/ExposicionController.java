472f42208293936a21a639bac96a1a2eb5fa4ff3
sanoreki123 <antony.pc2020@gmail.com>
2025-05-13 11:46:28 -0500

    creacion de las capas exposicion

---
+++ src/main/java/dsw/detodoartebackend/controller/ExposicionController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsw.detodoartebackend.controller;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ExposicionController {
+    
+}
