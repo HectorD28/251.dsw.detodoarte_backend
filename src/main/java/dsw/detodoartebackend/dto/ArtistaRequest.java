c512bbddb5c0704f9198fc14f637a0ac83bcfdbd
Lichal2108 <richard232108@gmail.com>
2025-05-12 19:04:35 -0500

    añadiendo clases artista

---
+++ src/main/java/dsw/detodoartebackend/dto/ArtistaRequest.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package dsw.detodoartebackend.dto;
+
+/**
+ *
+ * @author Richard
+ */
+public class ArtistaRequest {
+
+}
