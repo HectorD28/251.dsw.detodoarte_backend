ab1dc8efc082cae01919edb1a4c8e12064387169
sanoreki123 <antony.pc2020@gmail.com>
2025-05-14 13:14:06 -0500

    actualizar el obradearte cambios

--- src/main/java/dsw/detodoartebackend/dto/ObraDeArteResponse.java
+++ src/main/java/dsw/detodoartebackend/dto/ObraDeArteResponse.java
@@ -1,6 +1,8 @@
 package dsw.detodoartebackend.dto;
 
+import dsw.detodoartebackend.entity.Artista;
 import dsw.detodoartebackend.entity.ObraDeArte;
+import dsw.detodoartebackend.entity.Tecnica;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,8 +20,8 @@ public class ObraDeArteResponse {
     private String titulo;
     private LocalDate fechaRealizacion;
     private String dimensiones;
-    private Long tecnicaId;  // ID de la técnica
-    private Long artistaId;  // ID del artista
+    private Tecnica tecnicaId;  // ID de la técnica
+    private Artista artistaId;  // ID del artista
     private Double precio;
     private Integer cantidadVisualizacines;
 
@@ -29,8 +31,8 @@ public class ObraDeArteResponse {
             .titulo(obradearte.getTitulo())
             .fechaRealizacion(obradearte.getFechaRealizacion())
             .dimensiones(obradearte.getDimensiones())
-            .tecnicaId(obradearte.getTecnica().getId()) // Acceso a la técnica asociada
-            .artistaId(obradearte.getArtista().getIdArtista()) // Acceso al artista asociado
+            .tecnicaId(obradearte.getTecnica()) // Acceso a la técnica asociada
+            .artistaId(obradearte.getArtista()) // Acceso al artista asociado
             .precio(obradearte.getPrecio())
             .cantidadVisualizacines(obradearte.getCantidadVisualizaciones())
             .build();
