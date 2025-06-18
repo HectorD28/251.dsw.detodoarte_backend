7bde724c0398962c39134ff98116a5cc3cba3640
sanoreki123 <antony.pc2020@gmail.com>
2025-06-18 17:12:17 -0500

    mejoras.4

--- src/main/java/dsw/detodoartebackend/repository/CriterioEvaluacionesArtisticasRepository.java
+++ src/main/java/dsw/detodoartebackend/repository/CriterioEvaluacionesArtisticasRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CriterioEvaluacionesArtisticasRepository extends JpaRepository<CriterioEvaluacionesArtisticas, Long> {
+        // Método para verificar si ya existe una combinación de id_evaluacion_artistica e id_criterio_evaluacion_artistica
+    boolean existsByEvaluacionArtistica_IdEvaluacionArtisticaAndCriterioEvaluacionArtistica_IdCriterioEvaluacionArtistica(
+            Long idEvaluacionArtistica, Long idCriterioEvaluacionArtistica);
 }
